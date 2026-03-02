41e12c5880ad544b515a39c9a9e901d68b043cfc
saran <saranv740@gmail.com>
2026-03-02 22:44:10 +0530

    refactor:changed folder name

--- src/routes/post/[slug]/+page.ts
+++ src/routes/post/[slug]/+page.ts
@@ -5,7 +5,7 @@ import type { Component } from "svelte";
 
 export async function load({ params }: PageLoadEvent) {
 	try {
-		const post = await import(`$lib/contents/${params.slug}.md`);
+		const post = await import(`$lib/blog/${params.slug}.md`);
 		return {
 			content: post.default as Component,
 			meta: post.metadata as Post,
@@ -19,7 +19,7 @@ export async function load({ params }: PageLoadEvent) {
 export const entries: EntryGenerator = () => {
 	const slugs: Array<{ slug: string }> = [];
 
-	const paths = import.meta.glob("/src/lib/contents/*.md", { eager: true });
+	const paths = import.meta.glob("/src/lib/blog/*.md", { eager: true });
 	for (const path in paths) {
 		const slug = path.split("/").at(-1)?.replace(".md", "");
 		if (slug && slug !== "") {
